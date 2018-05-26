067cee3e3d4782bb35e85fea59726560af674d6f
Oleg Ferens <oleg.ferens@gmail.com>
2018-05-26 16:01:13 +0300

    WebRTC initialize

--- public/js/app.js
+++ public/js/app.js
@@ -25,4 +25,20 @@ formEl.form({
   },
 });
 
+// create our WebRTC connection
+const webrtc = new SimpleWebRTC({
+  // the id/element dom element that will hold "our" video
+  localVideoEl: 'local-video',
+  // the id/element dom element that will hold remote videos
+  remoteVideosEl: 'remote-videos',
+  // immediately ask for camera access
+  autoRequestMedia: true,
+});
+
+// We got access to local camera
+webrtc.on('localStream', () => {
+  localImageEl.hide();
+  localVideoEl.show();
+});
+
 });
